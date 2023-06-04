8dfb2a0815cda69998793e90967af43198bf0431
Mechislavas <Mechislavas@ecopsy.ru>
2023-06-04 20:03:11 +0300

    added cors

--- src/main/java/com/graduate/graduatework/config/ApplicationConfig.java
+++ src/main/java/com/graduate/graduatework/config/ApplicationConfig.java
@@ -3,8 +3,10 @@ package com.graduate.graduatework.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
+@Order(1)
 public class ApplicationConfig {
     @Bean
     public ModelMapper modelMapper() {
--- src/main/java/com/graduate/graduatework/config/SecurityConfig.java
+++ src/main/java/com/graduate/graduatework/config/SecurityConfig.java
@@ -1,14 +1,15 @@
 package com.graduate.graduatework.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@Order(4)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
